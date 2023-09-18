791650a4eb441a23c084f70ac8e00ff6b5747f7e
orangekame3 <miya.org.0309@gmail.com>
2023-09-18 13:10:20 +0900

    update

---
+++ diff.go
@@ -0,0 +1,78 @@
+package diffy
+
+import (
+	"fmt"
+
+	"github.com/kylelemons/godebug/diff"
+)
+
+// Ope defines the operation of a diff item.
+type Ope int8
+
+func (o Ope) String() string {
+	switch o {
+	case Delete:
+		return "@Delete"
+	case Add:
+		return "@Add"
+	case Equal:
+		return "@Equal"
+	}
+	panic(fmt.Sprintf("unknown value of Ope: %d", int(o)))
+}
+
+const (
+	// Delete item represents a delete diff.
+	Delete Ope = -1
+	// Insert item represents an insert diff.
+	Add Ope = 1
+	// Keep item represents an equal diff.
+	Equal Ope = 0
+)
+
+type Line struct {
+	Ope           Ope
+	NewLineNumber int
+	OldLineNumber int
+	Text          string
+}
+
+func FormatWithLineNumber(chunks []diff.Chunk) []Line {
+	var lines []Line
+	oldLineNumber, newLineNumber := 1, 1
+
+	for _, chunk := range chunks {
+		for _, text := range chunk.Deleted {
+			lines = append(lines, Line{
+				Ope:           Delete,
+				NewLineNumber: -1,
+				OldLineNumber: oldLineNumber,
+				Text:          text,
+			})
+			oldLineNumber++
+		}
+
+		for _, text := range chunk.Added {
+			lines = append(lines, Line{
+				Ope:           Add,
+				NewLineNumber: newLineNumber,
+				OldLineNumber: -1,
+				Text:          text,
+			})
+			newLineNumber++
+		}
+
+		for _, text := range chunk.Equal {
+			lines = append(lines, Line{
+				Ope:           Equal,
+				NewLineNumber: newLineNumber,
+				OldLineNumber: oldLineNumber,
+				Text:          text,
+			})
+			newLineNumber++
+			oldLineNumber++
+		}
+	}
+
+	return lines
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github/orangekame3/diffy
+
+go 1.21.0
+
+require github.com/kylelemons/godebug v1.1.0

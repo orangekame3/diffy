cefe02d4c49abca6161b5d9418f4970cb766188d
orangekame3 <miya.org.0309@gmail.com>
2023-09-18 13:09:08 +0900

    update

---
+++ example/main.go
@@ -0,0 +1,39 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"fmt"
+	"github/orangekame3/diffy"
+	"strings"
+
+	"github.com/kylelemons/godebug/diff"
+)
+
+func main() {
+	a := strings.TrimSpace(`
+	We the People of the United States, in Order to form a more perfect Union,
+	establish Justice, insure domestic Tranquility, provide for the common defence,
+	promote the general Welfare, and secure the Blessings of Liberty to ourselves
+	and our Posterity, do ordain and establish this Constitution for the United
+	States of America.
+	`)
+
+	b := strings.TrimSpace(`
+	:wq
+	We the People of the United States, in Order to form a more perfect Union,
+	establish Justice, insure domestic Tranquility, provide for the common defence,
+	and secure the Blessings of Liberty to ourselves
+	and our Posterity, do ordain and establish this Constitution for the United
+	States of America.
+	`)
+
+	aLines := strings.Split(a, "\n")
+	bLines := strings.Split(b, "\n")
+	chunks := diff.DiffChunks(aLines, bLines)
+	lines := diffy.FormatWithLineNumber(chunks)
+	for _, line := range lines {
+		fmt.Println(line.Ope, line.NewLineNumber, line.OldLineNumber, line.Text)
+	}
+
+}
